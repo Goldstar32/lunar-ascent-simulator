e5e400c4dd2f1f6d061ae9deffab538c087b8273
Goldstar32 <josua.karnsund@gmail.com>
2024-09-27 15:12:33 +0200

    Added property MassDry
    Doing some testing with adding variables to the rocket class.

--- Rocket.cs
+++ Rocket.cs
@@ -3,8 +3,9 @@ using System;
 
 public partial class Rocket : StaticBody3D
 {
-	
-	
+	public int MDry
+	{ get; set; }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
