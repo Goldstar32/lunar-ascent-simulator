a6ccc9dffc35fafb69199ffd3a5e9a9c171ad327
Goldstar32 <93144472+Goldstar32@users.noreply.github.com>
2024-10-08 15:59:34 +0200

    Added a rocket manually to test

--- PhysicsProcess.cs
+++ PhysicsProcess.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class PhysicsProcess : Node
 {
+	// Testing the rocket
+	private Rocket rocket = new(10, 10, new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Basis());
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
