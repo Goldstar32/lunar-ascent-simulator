346147cfb60dd867cd8f67c341cfdb7e45c4f65f
Goldstar32 <93144472+Goldstar32@users.noreply.github.com>
2024-10-08 16:02:15 +0200

    Fixed testrocket

--- PhysicsProcess.cs
+++ PhysicsProcess.cs
@@ -9,6 +9,11 @@ public partial class PhysicsProcess : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Set rocket starting position
+		rocket.Position = new Vector3(0, 0, 0);
+
+		// Add rocket to scene
+		AddChild(rocket);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
