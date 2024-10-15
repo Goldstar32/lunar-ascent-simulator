6abe62c1edfce9c2bd46ceac855d2bedfec2a437
Goldstar32 <93144472+Goldstar32@users.noreply.github.com>
2024-10-15 15:58:23 +0200

    Added methods
    Added methods for updating acceleration, velocity and position. Also added a temporary variable to account for gravity.

--- PhysicsProcess.cs
+++ PhysicsProcess.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class PhysicsProcess : Node
 {
+	// TEMPORARY variables for testing
+	private Vector3 gravity = new Vector3(0, -1.62f, 0);
+
 	// Rocket scene path
 	private PackedScene rocketScene;
 	private Rocket rocket;
@@ -22,6 +25,30 @@ public partial class PhysicsProcess : Node
 		AddChild(rocket);
 	}
 
+	// Forces will need to be calculated based on other factors (gravity by distance to moon and thrust by angle)
+
+	// Updates acceleration based on forces (Gravitation from moon (add thrust from rocket here later))
+	private void UpdateAcceleration(Rocket rocket)
+	{
+		Vector3 lastRes = rocket.MTot * rocket.Acceleration; // Resulting force on rocket before update
+		Vector3 newRes = gravity + lastRes; // New resulting force
+		Vector3 newAcc = newRes / rocket.MTot; // New acceleration
+
+		// Update rocket with new acceleration
+		rocket.Acceleration = newAcc;
+	}
+
+	// Updates velocity based on acceleration and time step
+	private void UpdateVelocity(Rocket rocket, double delta)
+	{
+		rocket.Velocity += rocket.Acceleration * (float)delta; // Velocity = acceleration * time since last update (as float)
+	}
+
+	// Updates position based on velocity and time step
+	private void UpdatePosition(Rocket rocket, double delta)
+	{
+		rocket.GlobalPosition += rocket.Velocity * (float)delta; // Position = velocity * time since last update (as float)
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
