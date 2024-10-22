49dc1ede39337ea3655ef1f066b52dc37bd282d3
Goldstar32 <93144472+Goldstar32@users.noreply.github.com>
2024-10-22 15:29:54 +0200

    Update Rocket.cs

--- Rocket.cs
+++ Rocket.cs
@@ -43,6 +43,7 @@ public partial class Rocket : StaticBody3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		RotationBasis = Basis.Identity;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
